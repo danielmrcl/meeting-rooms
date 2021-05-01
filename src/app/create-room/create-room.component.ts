f824d013d25afa1b82f75d0093b6d08453b3627d
Daniel Marcl <49128637+danielmrcl@users.noreply.github.com>
2021-05-01 04:01:46 -0300

    implementado o componente decriar sala

--- src/app/create-room/create-room.component.ts
+++ src/app/create-room/create-room.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Room } from '../room';
+import { RoomService } from '../room.service';
 
 @Component({
   selector: 'app-create-room',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-room.component.css']
 })
 export class CreateRoomComponent implements OnInit {
+  room: Room = new Room();
+  submitted: Boolean = false;
 
-  constructor() { }
+  constructor(private roomService: RoomService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  newRoom(): void {
+    this.submitted = false;
+    this.room = new Room();
+  }
+
+  save() {
+    this.roomService.createRoom(this.room).subscribe(
+      data => console.log(data),
+      error => console.log(error)
+    );
+    this.room = new Room();
+    this.goToList();
+  }
+
+  onSubmit() {
+    this.save();
+    this.submitted = true;
+  }
+
+  goToList() {
+    this.router.navigate(['rooms']);
+  }
 }
