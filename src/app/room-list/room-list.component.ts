d524dd5d2d6a2a23c003c6ca4766bebf3fc2a20e
Daniel Marcl <49128637+danielmrcl@users.noreply.github.com>
2021-05-01 04:02:59 -0300

    implementado o componente de listar as salas

--- src/app/room-list/room-list.component.ts
+++ src/app/room-list/room-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Room } from '../room';
+import { RoomService } from '../room.service';
 
 @Component({
   selector: 'app-room-list',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-list.component.css']
 })
 export class RoomListComponent implements OnInit {
+  rooms: any;
 
-  constructor() { }
+  constructor(private roomService: RoomService, private router: Router) { }
 
   ngOnInit(): void {
+    this.reloadData();
   }
 
+  reloadData() {
+    this.rooms = this.roomService.getRoomsList();
+  }
+
+  deleteRoom(roomId: number) {
+    this.roomService.deleteRoom(roomId).subscribe(
+      data => {
+        console.log(data);
+        this.reloadData();
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  roomDetails(roomId: number) {
+    this.router.navigate(['details', roomId]);
+  }
+
+  updateRoom(roomId: number) {
+    this.router.navigate(['update', roomId]);
+  }
 }
