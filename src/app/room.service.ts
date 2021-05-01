22c9f7359da03a3cba18ebe1776e6ff9474fe87f
Daniel Marcl <49128637+danielmrcl@users.noreply.github.com>
2021-05-01 04:04:17 -0300

    implementado a camada de serviço que se comunica com a API em java

--- src/app/room.service.ts
+++ src/app/room.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Room } from './room';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomService {
 
-  constructor() { }
+  private baseUrl: string = 'http://localhost:8080/api/rooms';
+
+  constructor(private http: HttpClient) { }
+
+  getRoomsList(): Observable<Object> {
+    return this.http.get(`${this.baseUrl}`);
+  }
+
+  deleteRoom(roomId: number): Observable<Object> {
+    return this.http.delete(`${this.baseUrl}/${roomId}`);
+  }
+
+  createRoom(room: Room): Observable<Object> {
+    return this.http.post(`${this.baseUrl}`, room);
+  }
+
+  getRoom(roomId: number): Observable<Object> {
+    return this.http.get(`${this.baseUrl}/${roomId}`);
+  }
+
+  updateRoom(roomId: number, room: Room): Observable<Object> {
+    return this.http.put(`${this.baseUrl}/${roomId}`, room);
+  }
 }
