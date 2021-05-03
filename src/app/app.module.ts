9a446fa38dd318e3d8c408a03c5de3bd6c9d9e84
Daniel Marcl <49128637+danielmrcl@users.noreply.github.com>
2021-05-03 00:51:11 -0300

    importaçoes necessarias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateRoomComponent } from './create-room/create-room.component';
 import { RoomDetailsComponent } from './room-details/room-details.component';
@@ -16,7 +18,10 @@ import { UpdateRoomComponent } from './update-room/update-room.component';
     UpdateRoomComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
